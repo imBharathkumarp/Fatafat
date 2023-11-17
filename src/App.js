50862b80b963e4ad7c8b7aefdd15b50a20c2fe6c
7BharathKumar <bharath2003kumarp@gmail.com>
2023-11-17 11:07:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header';
+import './App.css';
+import {Footer} from './components/Footer';
+import React from 'react';
+function App(){
+  return(
+    <div>
+      <Header/>
+    </div>
+
+  );
+}
+
+export default App;
